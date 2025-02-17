2e5253d61e710bf95061102f4fc306d0fcd29958
Ak David <120152054+shadowless16@users.noreply.github.com>
2025-02-17 23:43:35 +0100

    Update profile-data.js

--- JS/profile-data.js
+++ JS/profile-data.js
@@ -1,46 +1,46 @@
 const profileData = {
     user: {
-        // name: "Oluwaseun Adeyemi",
-        // avatar: "https://i.pravatar.cc/150?img=32",
-        // level: "300 Level",
-        // department: "Computer Science",
-        // bio: "Passionate programmer with experience in web and mobile development. Actively involved in open-source projects and tech community events.",
-        // memberSince: "January 2022",
-        // followers: 234,
-        // following: 145,
-        // postsCount: 32,
-        // friendsCount: 125,
-        // achievementsCount: 10
+        name: "Oluwaseun Adeyemi",
+        avatar: "https://i.pravatar.cc/150?img=32",
+        level: "300 Level",
+        department: "Computer Science",
+        bio: "Passionate programmer with experience in web and mobile development. Actively involved in open-source projects and tech community events.",
+        memberSince: "January 2022",
+        followers: 234,
+        following: 145,
+        postsCount: 32,
+        friendsCount: 125,
+        achievementsCount: 10
     },
     
     posts: [
-        // { 
-        //     image: "https://images.unsplash.com/photo-1519681393784-d120267933ba",
-        //     likes: 42,
-        //     comments: 7
-        // },
-        // { 
-        //     image: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
-        //     likes: 38,
-        //     comments: 5
-        // },
-        // { 
-        //     image: "https://images.unsplash.com/photo-1532634896-26909d0d4b6a",
-        //     likes: 56,
-        //     comments: 9
-        // }
+        { 
+            image: "https://images.unsplash.com/photo-1519681393784-d120267933ba",
+            likes: 42,
+            comments: 7
+        },
+        { 
+            image: "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+            likes: 38,
+            comments: 5
+        },
+        { 
+            image: "https://images.unsplash.com/photo-1532634896-26909d0d4b6a",
+            likes: 56,
+            comments: 9
+        }
     ],
 
     friends: [
-        // { 
-        //     name: "Chidi Okonkwo",
-        //     avatar: "https://i.pravatar.cc/150?img=33",
-        //     mutual: 15
-        // },
-        // { 
-        //     name: "Amina Bello",
-        //     avatar: "https://i.pravatar.cc/150?img=34",
-        //     mutual: 8
-        // }
+        { 
+            name: "Chidi Okonkwo",
+            avatar: "https://i.pravatar.cc/150?img=33",
+            mutual: 15
+        },
+        { 
+            name: "Amina Bello",
+            avatar: "https://i.pravatar.cc/150?img=34",
+            mutual: 8
+        }
     ]
 };
